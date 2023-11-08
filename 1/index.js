f2bf803cf9a2ea26d837e95b7ca14440dcb2a48c
06-developer <bahromrustamov4244@gmail.com>
2023-11-08 10:46:09 +0300

    1

---
+++ 1/index.js
@@ -0,0 +1,56 @@
+//HOMEWORK//
+
+//1.//
+// alert(5 > 4);//javobi: 5;
+// alert("apple" + "pineapple");//javobi: pinapple;
+//2.//
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+// console.log(a,b,c,d)//javobi: 2 2 2 1;
+//3.//
+// let a = 2;
+// let x = 1 +(a *= 2);
+// console.log(a,x)//javobi: 4 5;
+//4.//
+// let n = "" + 1 + 0;
+// console.log(n)//javobi: 10;
+//5.//
+// let n = "" - 1 + 0;
+// console.log(n)//javobi: -1;
+//6.//
+// let equal = true + false;
+// console.log(equal)//javobi: 1;
+//7.//
+// let plus = 6 / "3";
+// console.log(plus);//javobi: 2;
+//8.//
+// let a = "2" * "3";
+// console.log(a);//javobi: 6;
+//9.//
+// let a = 4 + 5 + "px";
+// console.log(a);//javobi: 9px;
+//10.//
+// let a = "$" + 4 + 5;
+// console.log(a)//javobi: $45;
+//11.//
+// let a = "4" - 2;
+// console.log(a);//javobi: 2;
+//12.//
+// let a = "4px" - 2;
+// console.log(a)//javobi: NaN;
+//13.//
+// let a = " -9 " + 5;
+// console.log(a)//javobi: -9 5;
+//14.//
+// let a = " -9 " - 5;
+// console.log(a);//javobi: -14;
+//15.//
+// let a = null + 1;
+// console.log(a)//javobi: 1;
+//16.//
+// let a = undefined + 1;
+// console.log(a);//javobi: NaN;
+//17.//
+// let a = " \t \n" - 2;
+// console.log(a)//javobi: -2;
